16d3da158b93827652c1773efbf0061c05f8dfef
Arin-star-light <daeung1010@gmail.com>
2023-11-24 09:25:10 +0900

    2023-11-24 upload

--- src/main/java/com/columbus/back/common/FavoriteListItem.java
+++ src/main/java/com/columbus/back/common/FavoriteListItem.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.columbus.back.entity.UserEntity;
+import com.columbus.back.repository.resultSet.GetFavoriteListResultSet;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,17 +16,16 @@ public class FavoriteListItem {
     private String nickname;
     private String profileImage;
 
-    public FavoriteListItem(UserEntity userEntity) {
-        this.userId = userEntity.getUserId();
-        this.nickname = userEntity.getNickname();
-        this.profileImage = userEntity.getProfileImageUrl();
-
+    public FavoriteListItem(GetFavoriteListResultSet resultSet) {
+        this.userId = resultSet.getUserId();
+        this.nickname = resultSet.getNickname();
+        this.profileImage =resultSet.getProfileImage(); 
     }
 
-    public static List<FavoriteListItem> getList(List<UserEntity> userEntities) {
+    public static List<FavoriteListItem> copyList(List<GetFavoriteListResultSet> resultSets) {
         List<FavoriteListItem> list = new ArrayList<>();
-        for (UserEntity userEntity: userEntities) {
-            FavoriteListItem favoriteListItem = new FavoriteListItem(userEntity);
+        for (GetFavoriteListResultSet resultSet: resultSets) {
+            FavoriteListItem favoriteListItem = new FavoriteListItem(resultSet);
             list.add(favoriteListItem);
         }
         return list;
